f0ce5d7c1cfa380cee27bd224e05b20ded1d25eb
Yaram <pootisspencerherenao@gmail.com>
2019-01-20 15:16:42 +1100

    Basic CLI

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,17 @@
+import * as program from 'commander';
 import BzzApi from '@erebos/api-bzz-node';
 
+program
+    .version('0.1.0')
+    .option('--bzzUrl [url]', 'Swarm HTTP API endpoint', 'https://swarm-gateways.net')
+    .parse(process.argv);
+
 const bzz = new BzzApi({
-    url: 'https://swarm-gateways.net'
+    url: program.bzzUrl
+});
+
+bzz.download('theswarm.eth').then((res) => {
+    return res.text();
+}).then((text) => {
+    console.log(text);
 });
