21d23ced8c31729b04ed02c04f322bc666234758
Yaram <pootisspencerherenao@gmail.com>
2019-01-20 18:10:23 +1100

    Basic init command

--- src/cli.ts
+++ src/cli.ts
@@ -1,17 +1,42 @@
 import * as program from 'commander';
-import BzzApi from '@erebos/api-bzz-node';
+import * as winston from 'winston';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.cli(),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
 
 program
     .version('0.1.0')
-    .option('--bzzUrl [url]', 'Swarm HTTP API endpoint', 'https://swarm-gateways.net')
-    .parse(process.argv);
+    .option('--repo [path]', 'local repository directory', '.');
 
-const bzz = new BzzApi({
-    url: program.bzzUrl
-});
+program
+    .command('init')
+    .description('initialize a new repository')
+    .action(() => {
+        const repositoryPath = path.resolve(program.repo);
 
-bzz.download('theswarm.eth').then((res) => {
-    return res.text();
-}).then((text) => {
-    console.log(text);
-});
+        if(!fs.existsSync(repositoryPath)){
+            logger.error(`Repository directory at ${repositoryPath} does not exist`);
+            process.exit(1);
+        }
+
+        const dataPath = path.join(repositoryPath, '.swarmvc');
+
+        if(fs.existsSync(dataPath)){
+            logger.error('A repository already exists in this directory');
+            process.exit(1);
+        }
+
+        fs.mkdirSync(dataPath);
+        fs.mkdirSync(path.join(dataPath, 'objects'));
+
+        logger.info(`Initialized repository in ${repositoryPath}`);
+    });
+
+program.parse(process.argv);
